b5393dbe48dd97177e53a5a3ca04f65e08083add
sakshamsingh07 <singhsaksham8235@gmail.com>
2022-12-02 04:36:09 +0530

    initial commit of sun file

---
+++ sub.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sub 
+{
+    public static int subb(int a , int b)
+    {
+        return a-b;
+    }
+    public static void main(String []args)
+    {
+        Scanner scn = new Scanner (System.in);
+        int a= scn.nextInt();
+        int b=scn.nextInt();
+        int ans=subb(a,b);
+        System.out.println(ans);
+    }
+
+    
+}
